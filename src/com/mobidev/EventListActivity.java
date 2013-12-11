79ad4415846f0a49f13fa5a1c240c3c849af76e8
jervenne <j0y5@hotmail.com>
2013-12-12 00:32:48 +0800

    changed logo + added onclick event to PollActivity

--- src/com/mobidev/EventListActivity.java
+++ src/com/mobidev/EventListActivity.java
@@ -55,23 +55,20 @@ public class EventListActivity extends Activity {
 
 			}
 		});
-		/**
+		
 		//click to update data
 		eventList.setOnItemClickListener(new OnItemClickListener() {
 
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
 
-				Intent i = new Intent(getApplicationContext(),
-						AddActivity.class);
-				i.putExtra("Fname", location.get(arg2));
-				i.putExtra("Lname", description.get(arg2));
-				i.putExtra("ID", eventID.get(arg2));
-				i.putExtra("update", true);
+				Intent i = new Intent(getApplicationContext(),PollActivity.class);
+				i.putExtra("emailAdd", email);
+				i.putExtra("evenID", eventID.get(arg2));
 				startActivity(i);
 
 			}
-		});**/
+		});
 	}
 	@Override
 	protected void onResume() {
@@ -109,71 +106,3 @@ public class EventListActivity extends Activity {
 	
 
 }
-
-
-/**
-import java.util.List;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.TextView;
-
-public class EventListActivity extends Activity {
-	
-	EventDAO eventDAO;
-	ImageButton imageButton;
-	ListView eventList;
-	List<Event> events;
-	TextView eventListLabel;
-	String email;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.event_list);
-		
-		//SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-		//String email = sharedPreferences.getString("email", "");
-		
-		Intent i = getIntent();
-		email = i.getStringExtra("emailAdd");
-        
-		eventDAO = new EventDAO(this);
-		
-		eventListLabel = (TextView) findViewById(R.id.eventListLabel);
-		imageButton = (ImageButton) findViewById(R.id.addImageButton);
-		eventList = (ListView) findViewById(R.id.eventList);
-		
-		events = eventDAO.getEventsOfAUser(eventDAO.getUser(email));
-		
-		if (events != null && events.size() > 0){
-			eventListLabel.setText("Scheduled Events");
-			ArrayAdapter<Event> adapter = new ArrayAdapter<Event>(this, R.layout.list_item, events);
-			eventList.setAdapter(adapter);
-		} else {
-			eventListLabel.setText("There are no events scheduled. Go on and create your first one!");
-		}
-		
-		imageButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Log.i("clicks","You clicked Add Event image button");
-    	        Intent i = new Intent(EventListActivity.this, ScheduleEventActivity.class);
-    	        i.putExtra("emailAdd", email);
-    	        startActivity(i);
-			}
-		});
-	}
-	
-}
-**/
-	
-
